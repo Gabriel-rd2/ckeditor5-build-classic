0e872c54c8bee526c480aec6010d2563674b1d28
Gabriel-rd2 <gabriel@rd2.ventures>
2021-09-24 10:57:44 -0300

    updating build

--- src/CardsConnectionPlugin/ui/view.js
+++ src/CardsConnectionPlugin/ui/view.js
@@ -2,13 +2,25 @@ import View from "@ckeditor/ckeditor5-ui/src/view";
 import ListView from "@ckeditor/ckeditor5-ui/src/list/listview";
 import ListItemView from "@ckeditor/ckeditor5-ui/src/list/listitemview";
 
+import "./theme.css";
 export default class CardsConnectionView extends ListView {
 	constructor(locale) {
 		super(locale);
 	}
 }
 
-export class CardsConnectionItemView extends ListItemView {}
+export class CardsConnectionItemView extends ListItemView {
+	constructor(locale) {
+		super(locale);
+
+		this.extendTemplate({
+			attributes: {
+				class: ["ck-cardconnection-list"],
+				tabindex: "-1",
+			},
+		});
+	}
+}
 
 export class DomWrapperView extends View {
 	constructor(locale, domElement) {
